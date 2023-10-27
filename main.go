1a0ddde69b696c585f044ef8e3caa68bb61ee493
vcokltfre <vcokltfre@gmail.com>
2023-10-27 18:07:58 +0100

    feat: create stub api

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/vcoltfre/godo/src/api"
+)
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+}
+
+func main() {
+	if err := api.Start(":8080"); err != nil {
+		logrus.WithError(err).Fatal("Failed to start API")
+	}
+}
